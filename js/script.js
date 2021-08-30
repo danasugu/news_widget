cc3b09168f665e5acdd3b67cb9ad74fc11158770
Dana Sugu <dana.sugu@gmail.com>
2021-08-30 20:37:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-function fetchData() {
-  fetch('http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data.news);
-    })
-    .catch((error) => {
-      console.log(error);
+'use strict';
+$.ajax({
+  url: 'http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8',
+  dataType: 'json',
+  type: 'GET',
+  cache: false,
+  data: {
+    minRow: 1,
+    maxRow: 5,
+  },
+  success: function (data) {
+    $(data.news).each(function (index, value) {
+      $('ul').append('<li class="title">' + value.title + '</li>');
+      $('ul').append('<li class="details">' + value.details + '</li>');
     });
-}
+  },
+});
 
-fetchData();
-
-// 'use strict';
-// $.ajax({
-//   url: 'http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8',
-//   dataType: 'json',
-//   type: 'GET',
-//   cache: false,
-//   data: {
-//     minRow: 1,
-//     maxRow: 5,
-//   },
-//   success: function (data) {
-//     $(data.news).each(function (index, value) {
-//       $('ul').append('<li class="title">' + value.title + '</li>');
-//       $('ul').append('<li class="details">' + value.details + '</li>');
+// function fetchData() {
+//   fetch('http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8')
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data.news);
+//     })
+//     .catch((error) => {
+//       console.log(error);
 //     });
-//   },
-// });
+// }
+
+// fetchData();

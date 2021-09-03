306c4a28d7288de239c6cdae0cb24f0b6b296a8b
Dana Sugu <dana.sugu@gmail.com>
2021-09-03 07:31:00 +0300

    cont

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 'use strict';
-$.ajax({
-  url: 'http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8',
-  dataType: 'json',
-  type: 'GET',
-  cache: false,
-  data: {
-    page: 1,
-    pagelimit: 5,
-  },
-  success: function (data) {
-    $(data.news).each(function (index, value) {
-      $('ul').append('<li class="title">' + value.title + '</li>');
-      $('ul').append('<li class="details">' + value.details + '</li>');
-    });
-  },
-  error: function (jqXHR, textStatus, errorThrown) {
-    console.log(jqXHR);
-    console.log(textStatus);
-    console.log(errorThrown);
-  },
-});
+const API_URL = 'http://www.mocky.io/v2/58fda6ce0f0000c40908b8c8';
+const loadPageOneElement = document.querySelector('.news-content');
+// const PageTwoButton = document.querySelector('.goTwoButton');
+// const pages = document.querySelector('.dots');
 
+async function getNews() {
+  loadPageOneElement.innerHTML = '';
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  const pageOne = json.news.slice(0, 5);
+  const pageTwo = json.news.slice(5, 10);
+  const pageThree = json.news.slice(10, 15);
 
+  pageOne.forEach((titleItem) => {
+    loadPageOneElement.innerHTML += `
+    <h3 class="news-title">${titleItem.title}</h3>
+    <p class="news-details">${titleItem.details}</p>`;
+
+    const titleElement = document.createElement('h3');
+    titleElement.classList.add('news-title');
+    titleElement.title = titleItem;
+
+    loadPageOneElement.appendChild(titleElement);
+  });
+}
+getNews();
